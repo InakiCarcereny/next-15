2dc8c1c8eea2855030796734579acf8efa715d48
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-02 21:00:19 -0300

    .

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -5,8 +5,8 @@ export default function Products() {
     <div>
       <h1>Products</h1>
       <Link href="/products/1">product 1</Link>
-      <li>product 2</li>
-      <li>product 3</li>
+      <Link href="/products/2">product 2</Link>
+      <Link href="/products/3">product 3</Link>
     </div>
   );
 }
